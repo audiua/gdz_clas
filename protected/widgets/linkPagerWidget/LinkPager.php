4057e0f28202e6a77a397a4807a89ebfbe03094c
audiua <audiua@yandex.ru>
2016-02-22 20:14:47 +0200

    fix

--- protected/widgets/linkPagerWidget/LinkPager.php
+++ protected/widgets/linkPagerWidget/LinkPager.php
@@ -128,17 +128,7 @@ class LinkPager extends CLinkPager
 	 */
 	public function run()
 	{
-		echo '<div class="clearfix"></div>';
-		echo '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- my_netboard_middle -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:580px;height:400px"
-     data-ad-client="ca-pub-9657826060070920"
-     data-ad-slot="2539516294"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>';
-		echo '<div class="clearfix"></div>';
+
 		$this->registerClientScript();
 		// echo '<noindex>';
 		$buttons=$this->createPageButtons();
